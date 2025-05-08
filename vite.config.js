21b94ce8e3e72a23d8fd55499ddf0a42ea79f41b
Mike <mikeunknown333@gmail.com>
2025-05-08 20:50:53 +0400

    added autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
+import autoprefixer from "autoprefixer";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -11,6 +12,11 @@ export default defineConfig({
                 main: resolve(__dirname, "index.html"),
                 sign_up: resolve(__dirname, "sign-up.html"),
             },
+        },
+    },
+    css: {
+        postcss: {
+            plugins: [autoprefixer()],
         },
     },
 });
